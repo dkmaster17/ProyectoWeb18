310925f608339273431b1d91539d1a14dbf71de8
Gustavo A. Florez B <dkmaster17@hotmail.com>
2017-10-12 12:33:38 -0300

    InitFiles

---
+++ CodeXD.php
@@ -0,0 +1,15 @@
+<?php
+
+$_Nombre=$_POST['nombre'];
+$_Apellido=$_POST['apellido'];
+$_Cedula=$_POST['cedula'];
+$_Direccion=$_POST['direccion'];
+$_Sexo=$_POST['sexo'];
+$_Telefono=$_POST['telefono'];
+$_Correo=$_POST['nickname'];
+$_Contraseña=$_POST['contraseña'];
+
+printf("Nombre: %s %s",$_Nombre,$_Sexo);
+
+
+?>
